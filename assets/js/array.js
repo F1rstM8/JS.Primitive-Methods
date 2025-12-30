3e0b1cffa1a11ea271572783f5189d370076764e
Maksym <praniknumberone@gmail.com>
2025-12-30 05:00:15 +0100

    Done task 5: Case insensitive compare

---
+++ assets/js/array.js
@@ -0,0 +1,6 @@
+function isEqual(str1, str2) {
+    return str1.toLowerCase() === str2.toLowerCase();
+}
+console.log(isEqual('pApA', 'papa'));     
+console.log(isEqual('qwerty', 'QWErty')); 
+console.log(isEqual('aaa', 'EEE'));       
