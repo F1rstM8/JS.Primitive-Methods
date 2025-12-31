3d468bb43de7b652bccff845fa47019ce1cf5c23
Maksym <praniknumberone@gmail.com>
2025-12-31 03:58:32 +0100

    Saving work before switching

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,7 +1,3 @@
-
-
-
-
 console.log("--- ЗАВДАННЯ 1 ---");
 const originalString = "Hello World";
 
@@ -9,32 +5,32 @@ const reversedString = originalString.split("").reverse().join("");
 
 console.log(reversedString);
 
-console.log("--- ЗАВДАННЯ 2 ---");
-function getIntegerPart(number) {
-  return Math.trunc(number);
-}
-console.log(getIntegerPart(5.95)); 
-console.log(getIntegerPart(-3.4)); 
+// console.log("--- ЗАВДАННЯ 2 ---");
+// function getIntegerPart(number) {
+//   return Math.trunc(number);
+// }
+// console.log(getIntegerPart(5.95));
+// console.log(getIntegerPart(-3.4));
 
 console.log("--- ЗАВДАННЯ 3 ---");
 const userName = prompt("Введіть ваше ім'я:");
 if (userName) {
-    alert(userName.toUpperCase()); 
+  alert(userName.toUpperCase());
 } else {
-    alert("Ви не ввели ім'я");
+  alert("Ви не ввели ім'я");
 }
 
 console.log("--- ЗАВДАННЯ 4 ---");
 function formatDate(dateString) {
-    const parts = dateString.split('-'); 
-    return parts[1] + "." + parts[2] + "." + parts[0];
+  const parts = dateString.split("-");
+  return parts[1] + "." + parts[2] + "." + parts[0];
 }
-console.log(formatDate("2021-22-09")); 
+console.log(formatDate("2021-22-09"));
 
 console.log("--- ЗАВДАННЯ 5 ---");
 function isEqual(str1, str2) {
-    return str1.toLowerCase() === str2.toLowerCase();
+  return str1.toLowerCase() === str2.toLowerCase();
 }
-console.log(isEqual('pApA', 'papa'));     
-console.log(isEqual('qwerty', 'QWErty')); 
-console.log(isEqual('aaa', 'EEE'));       
+console.log(isEqual("pApA", "papa"));
+console.log(isEqual("qwerty", "QWErty"));
+console.log(isEqual("aaa", "EEE"));
